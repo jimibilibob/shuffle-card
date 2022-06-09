62e5d84399f9424dabb4262f8e48092fe90e0e94
Israel Aguilar <isra.neri2@gmail.com>
2022-06-09 07:18:22 -0700

    Initial design is done

---
+++ shuffled cards app/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  shuffled cards app
+//
+//  Created by user on 9/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func shuffleCards(_ sender: Any) {
+        
+    }
+    
+}
+
---
+++ shuffled cards app/cells/CardCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CardCollectionViewCell.swift
+//  shuffled cards app
+//
+//  Created by user on 9/6/22.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
